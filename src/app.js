673555af6c57f2e19eb7f8e4444a6fd3da6d5abf
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-08-31 22:16:31 -0400

    setup switch component property from react-router-dom. setup handling of not found pages

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
  
@@ -28,15 +28,21 @@ const HelpPage = () => (
   help page
   </div>
 )
+const NotFoundPage = () => (
+  <div>
+  page not found
+  </div>
+)
+
   const Routes = (
     <BrowserRouter>
-    <div>
+    <Switch>
     <Route path="/"  component={ExpenseDashboardPage} exact={true}/>
     <Route path="/create" component={AddExpensePage} />
     <Route path="/edit" component={EditExpensePage} />
     <Route path="/help" component={HelpPage} />
-
-    </div>
+    <Route  component={NotFoundPage} />
+    </Switch>
     
     
     </BrowserRouter>
