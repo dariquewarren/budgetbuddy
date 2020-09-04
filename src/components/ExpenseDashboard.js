753b30821e382419cd9b5f73dd402009e0a8a941
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-09-04 02:43:37 -0400

    retrieved filter text from store. changed text filter via dispach call

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ExpenseList from './ExpenseList'
-
+import ExpenseListFilters from './ExpenseListFilters'
 
 const ExpenseDashboard = ()=>(
     <div>
     this is from my dash component
-
+    <ExpenseListFilters/>
     <ExpenseList/>
+
     </div>
     )
 
