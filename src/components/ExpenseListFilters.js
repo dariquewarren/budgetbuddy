ba0ac208b4b8734ed551a6cb02053aefca0873b4
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-09-04 04:16:47 -0400

    setup sortby by another means

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {setTextFilter, sortByAmount, sortByDate} from '../actions/filters'
-const ExpenseListFilters =(props)=>(
-    <div>
-    <input type="text" value={props.filters.text} onChange={(e)=>{
-       props.dispatch(setTextFilter(e.target.value))
-       
-        console.log(e.target.value)
-    }} />
-    <select value={props.filters.sortBy} onChange={((e)=>{
-        {e.target.value === 'amount' ? props.dispatch(sortByAmount()) : props.dispatch(sortByDate()) }
-    })}>
-    <option value='date'>Date</option>
-    <option value='amount'>Amount</option>
-    </select>
-    </div>
-)
-// setup value and onchane for select
-const mapStateToProps = (state)=>{
-    return {
-        filters: state.filters
-    }
-}
+import React from "react";
+import { connect } from "react-redux";
+import { setTextFilter, sortByAmount, sortByDate } from "../actions/filters";
+const ExpenseListFilters = (props) => (
+  <div>
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => {
+        props.dispatch(setTextFilter(e.target.value));
 
-export default connect(mapStateToProps)(ExpenseListFilters)
+        console.log(e.target.value);
+      }}
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+       if(e.target.value === 'amount'){
+           props.dispatch(sortByAmount())
+       }else if(e.target.value === 'date'){
+        props.dispatch(sortByDate())
+
+       }
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
+  </div>
+);
+// setup value and onchane for select
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters,
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseListFilters);
