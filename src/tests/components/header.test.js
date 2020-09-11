a0d74a82e5f57eaf8aaa5598b664e4deb663ba1d
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-09-11 14:51:41 -0400

    installed and setup enzyme, enzyme react16 adapter and raf. created jest.config.jso file to run raf and the setuptests file. created a setuptests file with enzyme adapter running inside it. connected setuptests file to jest via package .json

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,11 +1,13 @@
 // react test renderer
 import React from 'react'
+import {shallow} from 'enzyme'
 import Header from '../../components/Header'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
-
 
 test("should render headercmpnt", ()=>{
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header/>)
-    console.log(renderer.getRenderOutput())
+    const wrapper = shallow(<Header/>)
+    
+    expect(wrapper.find('h1').text()).toBe('Budget Buddy')
+    // const renderer = new ReactShallowRenderer()
+    // renderer.render(<Header/>)
+    // expect(renderer.getRenderOutput()).toMatchSnapshot()
 })
