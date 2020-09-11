9c2f33aecfbcb56ac2efdf7f4da775b7ee6150cc
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-09-11 15:08:21 -0400

    NOTE: REACT 16; STATELESS FUNCTIONAL COMPONENTS NO LONGER HAVE PROPS AUTOMATICALLY. MUST CALL.PROPS() ON WRAPPER WHEN EXPECTING IT

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,13 +1,16 @@
 // react test renderer
-import React from 'react'
-import {shallow} from 'enzyme'
-import Header from '../../components/Header'
+import React from "react";
+import { shallow } from "enzyme";
+import toJSON from 'enzyme-to-json'
+import Header from "../../components/Header";
 
-test("should render headercmpnt", ()=>{
-    const wrapper = shallow(<Header/>)
-    
-    expect(wrapper.find('h1').text()).toBe('Budget Buddy')
-    // const renderer = new ReactShallowRenderer()
-    // renderer.render(<Header/>)
-    // expect(renderer.getRenderOutput()).toMatchSnapshot()
-})
+test("should render headercmpnt", () => {
+  const wrapper = shallow(<Header />);
+// console.log(wrapper.props())
+
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  // expect(wrapper.find('h1').text()).toBe('Budget Buddy')
+  // const renderer = new ReactShallowRenderer()
+  // renderer.render(<Header/>)
+  // expect(renderer.getRenderOutput()).toMatchSnapshot()
+});
